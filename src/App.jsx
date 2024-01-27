ca08634478132e764d3f1f38a5feb96bdcef102c
MD ABDULLAH <mdabdullah161036@gmail.com>
2024-01-27 11:37:59 +0600

    google pupUp sign In and out

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import "./App.css";
+import app from "./firebase/firebase.init";
+
+function App() {
+  const [user, setUser] = useState(null);
+  const auth = getAuth(app);
+  const googleProvider = new GoogleAuthProvider();
+
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        const loggedUser = result.user;
+        setUser(loggedUser);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser(null);
+      })
+      .catch((error) => {
+        console.log(errorf);
+      });
+  };
+  return (
+    <div>
+      <h1>Dimple_Firebase + React</h1>
+      <div className="card">
+        {user && <h1>Name: {user.displayName}</h1>}
+
+        <div>
+          {user ? (
+            <button onClick={handleSignOut}>SignOut</button>
+          ) : (
+            <div>
+              <button onClick={handleGoogleSignIn}>Google </button>
+              <button>GitHub</button>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDF2SA0iedr60X8r5S4bt6SAh83Sg47mzs",
+  authDomain: "dimple-firebase-b7.firebaseapp.com",
+  projectId: "dimple-firebase-b7",
+  storageBucket: "dimple-firebase-b7.appspot.com",
+  messagingSenderId: "764444051900",
+  appId: "1:764444051900:web:12b76407b509e36c4983b9",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
